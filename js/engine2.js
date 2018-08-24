cb0bf2de5306cabcf2c0cb368496b434672f09f5
allanjames <james.allan@brafton.com>
2018-08-24 11:21:51 -0400

    basic working prototype, functions on all 3 browsers

--- js/engine2.js
+++ js/engine2.js
@@ -9,8 +9,7 @@
         var download = document.getElementById('download');
         var background = document.getElementById('background');
         var context = canvas.getContext('2d');
-        var svg = document.getElementById('bkgrnd');
-        var finish = document.getElementById('drawing');
+        var svg = document.getElementById('Capa_1');
         var posX = new Array();
         var posY = new Array();
         var posDrag = new Array();
@@ -18,6 +17,11 @@
         var engaged = false;
         var drawingColor = "rgba(0,200,0,1)";
         var colorChoices = document.getElementsByClassName('color');
+        var img = new Image();
+        img.onload = function() {
+            context.drawImage(img, 100, 100,400,400);
+        }
+        img.src = "goat.svg";
         for(var c = 0; c < colorChoices.length; c++){
             colorChoices[c].addEventListener('click',function(){
                 drawingColor = this.getAttribute('data-color');
@@ -33,63 +37,33 @@
             resetCanvas();
         });
         snapshot.addEventListener('click', function(){
-
-            //image.src = svg.toDataURL("image/svg");
-            var s = new XMLSerializer().serializeToString(document.getElementById("bkgrnd"));
-            var encodedData = window.btoa(s);
-            var newSrc = 'data:image/jpg;base64,'+encodedData;
             var image = new Image();
-            image.src = newSrc;
-            console.log(svg);
-            // var serializer = new XMLSerializer();
-            // var source = serializer.serializeToString(svg);
-            var background = document.getElementById("bkgrnd");
-            context.globalCompositeOperation='destination-over';
-            context.drawImage(image,0,0); 
-            // try{ 
-            //     context.drawImage(image,0,0); 
-            //     console.log('no error drawing image');
-            // } catch {
-            //     console.log('error drawing image');
-            // }  
-        });
-        finish.addEventListener('click',function(){
-            var finished = new Image();
-	        finished.src = canvas.toDataURL("image/png");
+            image.src = canvas.toDataURL("image/png");
             var serializer = new XMLSerializer();
-            var source = serializer.serializeToString(svg);
-            download.setAttribute('src', finished.src);
-        });
-        toggleCanvas.addEventListener('click',function(){
-            console.log(window.navigator.userAgent);
-            var s = new XMLSerializer().serializeToString(svg);
-            var encodedData = window.btoa(s);
-            var newSrc = 'data:image/svg+xml,'+encodedData;
-            var img = new Image();
-            img.onload = function() {
-                context.drawImage(img, 100, 100,400,400);
+            console.log(image.src); 
+            //var source = serializer.serializeToString(svg);
+            download.setAttribute('s rc', image.src);
+            
+            //console.log(source);
+         });
+         toggleCanvas.addEventListener('click',function(){
+            if(canvas.style.display=="block"){
+                canvas.style.display = "none";
+            } else{
+                canvas.style.display = "block";
             }
-            img.src = "data:image/svg+xml;base64,"+encodedData;
-            console.log(encodedData);
-            canvas.addEventListener('mousedown', function(e){
-                console.log(e);
-                engaged = true;
-                console.log(e.pageX,this.offsetLeft);
-                var mouseX = e.pageX-this.offsetLeft;
-                var mouseY = e.pageY-this.offsetTop;
-                recordPosition(mouseX, mouseY, false);
-                drawLine();
-            });
-            // if(canvas.style.display=="block"){
-            //     canvas.style.display = "none";
-            // } else{
-            //     canvas.style.display = "block";
-            // }
         });
         clear.addEventListener('click',function(){
             clearValues();
         });
-        
+        /*canvas.addEventListener('mousedown', function(e){
+            engaged = true;
+            console.log(e.pageX,this.offsetLeft);
+            var mouseX = e.pageX-this.offsetLeft;
+            var mouseY = e.pageY-this.offsetTop;
+            recordPosition(mouseX, mouseY, false);
+            drawLine();
+        });*/
         canvas.addEventListener('mousemove',function(e){
             if(engaged){
                 recordPosition(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
@@ -112,7 +86,9 @@
             console.log('attempting reset');
             clearValues();
             for(var a = 0; a < path.length; a++){
-                path[a].setAttribute('style','fill: rgba(0,0,0)');
+                try{
+                    path[a].setAttribute('style','fill: rgba(0,0,0)');
+                } catch { console.log('error'); }
             }
         }
         function clearValues(){
